5c67fbab8970a8be2c2fcfb7029a0b86bd960431
LoloO6 <ml12365@auca.kg>
2025-12-12 15:09:24 +0600

    chnaged init

---
+++ project-generator/project_generator/__init__.py
@@ -0,0 +1,9 @@
+"""Project Template Generator - Create structured project templates."""
+
+__version__ = "1.0.0"
+__author__ = "Your Name"
+
+from .generator import ProjectGenerator
+from .templates import TEMPLATES
+
+__all__ = ["ProjectGenerator", "TEMPLATES"]
